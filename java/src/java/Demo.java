3ce86d9c63f07487d5e581e764d9cfa0814f6f98
sreeja <sreejaanumulapuri301@gmail.com>
2026-05-19 04:09:27 +0000

    Add some file saved

---
+++ java/src/java/Demo.java
@@ -0,0 +1,9 @@
+package java;
+
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("This is a demo class.");
+		System.out.println("It demonstrates basic Java syntax.");
+	}
+
+}
--- java/src/java/HelloWorld.java
+++ java/src/java/HelloWorld.java
@@ -1,8 +1,14 @@
 package java;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 		public static void main(String[] args) {
 		System.out.println("Hello, World!");
+		System.out.println("Welcome to Java programming.");
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please enter your name: ");
+		
 	}
 
 }

e60bcfd26f65eff4dd93899f3edb268084729c3a
sreeja <sreejaanumulapuri301@gmail.com>
2026-05-19 04:49:51 +0000

    All done

---
+++ java/src/java/Student.java
@@ -0,0 +1,12 @@
+package java;
+
+public class Student {
+	public static void main(String[] args) {
+		System.out.println("This is a Student class.");
+		System.out.println("It represents a student in a school.");
+		System.out.println("A student has attributes like name, age, and grade.");
+		System.out.println("You can create student objects to store information about individual students.");
+		
+	}
+
+}
